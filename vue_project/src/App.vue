31f4ce0cb9d32825af2e3a47df48264378a41170
monxy747 <monxy.official@gmail.com>
2023-06-05 01:59:58 +0900

    first commit

---
+++ vue_project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <input @click="helloWorld" type="button" value="HelloWorld">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    helloWorld() {
+      this.$axios.get('http://localhost:8080/hello')
+        .then((response) => {
+          alert(response.data);
+        })
+        .catch((e) => {
+          alert(e);
+        });
+    }
+  }
+}
+</script>
---
+++ vue_project/src/main.js
@@ -0,0 +1,10 @@
+// main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+import axios from 'axios';
+
+const app = createApp(App);
+app.config.productionTip = false;
+app.config.globalProperties.$axios = axios;
+
+app.mount('#app');
